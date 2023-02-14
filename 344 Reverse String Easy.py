500dbaf3cf2dff521f7ee1c2d3077172dcad4e74
Alex Shapalov <shapalov@gmail.com>
2023-02-13 18:56:28 -0800

    344

--- 344 Reverse String Easy.py
+++ 344 Reverse String Easy.py
@@ -2,7 +2,7 @@
 
 # Write a function that reverses a string. The input string is given as an array of characters s.
 
-#  You must do this by modifying the input array in-place with O(1) extra memory.
+# You must do this by modifying the input array in-place with O(1) extra memory.
 
 # Input: s = ["h","e","l","l","o"]
 # Output: ["o","l","l","e","h"]
