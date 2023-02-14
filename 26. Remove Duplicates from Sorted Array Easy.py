afc4b20923243a37481734d9198ab63587f54a03
Alex Shapalov <shapalov@gmail.com>
2023-02-13 18:56:18 -0800

    26

--- 26. Remove Duplicates from Sorted Array Easy.py
+++ 26. Remove Duplicates from Sorted Array Easy.py
@@ -16,19 +16,55 @@
 
 # print(Solution().removeDuplicates(nums))
 
+# class Solution:
+# 	def removeDuplicates(self, nums: list[int]) -> int:
+# 		size = len(nums)
+# 		insertIndex = 1
+# 		for i in range(1, size):
+# 			# Found unique element
+# 			if nums[i - 1] != nums[i]:
+# 				# Updating insertIndex in our main array
+# 				nums[insertIndex] = nums[i]
+# 				# Incrementing insertIndex count by 1
+# 				insertIndex = insertIndex + 1
+# 		return insertIndex
+
+# nums = [1,1,2,2,3,3]
+
+# print(Solution().removeDuplicates(nums))
+
+
+# Calculate unique element
+# class Solution:
+# 	def removeDuplicates(self, nums: list[int]) -> int:
+# 		size = len(nums)
+# 		countIndex = 1
+# 		for i in range(1, size):
+# 			if nums[i - 1] != nums[i]:
+# 				print(nums[i-1], nums[i], countIndex)
+# 				nums[countIndex] = nums[i]
+# 				countIndex += 1
+# 		return countIndex
+
+# nums = [1,1,1,1,1,1,1,3,3,4,4,4,5,5,5]
+
+# print(Solution().removeDuplicates(nums))
+
+
+# Increment i index by 1 till we reach end of the array
+
+# Note: After reaching the end of the array, our insertIndex variable will hold the count of unique elements in our input array.
 class Solution:
 	def removeDuplicates(self, nums: list[int]) -> int:
 		size = len(nums)
-		insertIndex = 1
+		countIndex = 1
 		for i in range(1, size):
-			# Found unique element
 			if nums[i - 1] != nums[i]:
-				# Updating insertIndex in our main array
-				nums[insertIndex] = nums[i]
-				# Incrementing insertIndex count by 1
-				insertIndex = insertIndex + 1
-		return insertIndex
+				print(nums[i-1], nums[i], countIndex)
+				nums[countIndex] = nums[i]
+				countIndex += 1
+		return countIndex
 
-nums = [1,1,2,2,3,3]
+nums = ['a', 'b', 'c', 'c']
 
 print(Solution().removeDuplicates(nums))
